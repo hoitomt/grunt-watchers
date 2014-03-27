7935e7060d114f66dc2e6e333c647820bb9a50ac
Mike Hoitomt <mike.hoitomt@gmail.com>
2014-03-26 21:24:41 -0500

    Add sass, coffee, and haml

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,19 +3,76 @@ module.exports = function(grunt){
     connect: {
       server: {
         options: {
-          base: './',
+          base: './public',
           port: '4000',
           host: '*'
         }
       }
     },
+    clean: {
+      build: {
+        src: ['public/javascripts', 'public/css']
+      }
+    },
+    coffee: {
+      compile: {
+        options: {
+          bare: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'app/coffeescript',
+          src: ['**/*.coffee', '**/*.js'],
+          dest: 'public/javascripts',
+          ext: '.js'
+        }]
+      }
+    },
+    sass: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'app/stylesheets',
+          src: ['**/*.scss'],
+          dest: 'public/stylesheets',
+          ext: '.css'
+        }]
+      }
+    },
+    haml: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'app/views',
+          src: ['**/*.haml'],
+          dest: 'public',
+          ext: '.html'
+        }]
+      }
+    },
     watch: {
-
+      coffee: {
+        files: ['app/coffeescript/**/*.coffee'],
+        tasks: ['coffee']
+      },
+      styles: {
+        files: ['app/stylesheets/**/*.scss'],
+        tasks: ['sass']
+      },
+      haml: {
+        files: ['app/views/**/*.haml'],
+        tasks: ['haml']
+      }
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-haml');
 
-  grunt.registerTask('default', ['connect', 'watch']);
+  grunt.registerTask('build', ['clean', 'coffee', 'sass', 'haml']);
+  grunt.registerTask('default', ['build', 'connect', 'watch']);
 };
