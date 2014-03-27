74b68a6a225a88751d0217f42276896a5e34363f
Mike Hoitomt <mike.hoitomt@gmail.com>
2014-03-26 22:49:27 -0500

    Update README.md

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt){
     },
     clean: {
       build: {
-        src: ['public/javascripts', 'public/css']
+        src: ['public/javascripts', 'public/stylesheets']
       }
     },
     coffee: {
